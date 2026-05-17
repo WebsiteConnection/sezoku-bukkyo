5a2d4ac0a3939a8f6e74e4b03381443f1ef9f0af
WebsiteConnection <jrtate1947@gmail.com>
2026-05-17 21:18:48 +0900

    Update index.ts

--- src/config/index.ts
+++ src/config/index.ts
@@ -34,11 +34,11 @@ export const SITE_CONTENT: SiteContent = {
     },
     {
       name: "2. Naturalistic Buddhism",
-      summary: "Gil Fronsdal's exploration of a non-supernatural Dharma rooted in the early teachings with links to his '7 a.m. sits.'",
+      summary: "Gil Fronsdal's exploration of a non-supernatural Dharma rooted in the early teachings with links to his Audio Dharma",
       image: "/gil.png",
       links: [
         { text: "Insight Center", href: "https://www.insightmeditationcenter.org/" },
-        { text: "Audio Dharma", href: "https://www.audiodharma.org/" }
+        { text: "Gil's Audio Dharma", href: "https://www.audiodharma.org/speakers/1" }
       ]
     },
     {
