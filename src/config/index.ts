312035f3539cdde7e3774eff469e5e5edf4c8e32
WebsiteConnection <jrtate1947@gmail.com>
2026-05-19 01:44:59 +0000

    Update index.ts

--- src/config/index.ts
+++ src/config/index.ts
@@ -104,7 +104,7 @@ export const SITE_CONTENT: SiteContent = {
     },
   ],
   about: {
-  description: `The Secular Buddhism Hub is a curated directory designed to serve as a central location for the diverse voices of the secular Buddhist movement. From the historical roots of Dr. Ambedkar's teachings in India to modern naturalistic and existential interpretations in the West, this hub provides a neutral space for teachers and groups to introduce an online presence. The goal is to group these resources together so that anyone interested in this new category of Buddhism can compare the current options. For further information, contact the email address below.`,
+  description: `The Secular Buddhism Hub is a curated directory designed to serve as a central location for the diverse voices of the secular Buddhist movement. From the historical roots of Dr. Ambedkar's teachings in India to modern naturalistic and existential interpretations in the West, this hub provides a neutral space for teachers and groups to introduce an online presence. The goal is to present these resources together so that anyone interested in this emerging branch of Buddhism can compare the current offerings. For further information, contact the email address below.`,
   image: "/about wheel.png",
 },
 };
