e30a9a6418884651e28696f68c82772e4cc6e712
WebsiteConnection <jrtate1947@gmail.com>
2026-05-21 19:18:55 +0900

    Update index.ts

--- src/config/index.ts
+++ src/config/index.ts
@@ -24,53 +24,17 @@ export const SITE_CONTENT: SiteContent = {
     email: "SecularLotus@gmail.com",
   },
   sites: [
-    {
-      name: "1. The Secular Buddhist Network（世俗仏教ネットワーク）",
-      summary: "スティーブン・バチェラーやウィントン・ヒギンズの基礎的な著作に触発され、現代における「世俗的なダルマ（法）」への転換を牽引しています。その活動グループは欧米諸国に広がっています。",
-      image: "/sbn.png", 
+     {
+      name: "1. 法華伝統における世俗仏教",
+      summary: "法華経のクライマックスである「久遠実成の仏（永遠のブッダ）」の開顕を、内在に根ざした普遍的倫理として再解釈し、それに対応する世俗的実践のための祈りを紹介しています。",
+      image: "/lotus.png",
       links: [
-        { text: "公式サイト", href: "https://secularbuddhistnetwork.org/" },
-        { text: "コミュニティ", href: "https://secularbuddhistnetwork.org/the-sbn-community/" }
+        { text: "世俗の蓮", href: "https://sezoku-hokke.vercel.app/" },
+        { text: "English", href: "https://secular-lotus.vercel.app/" }
       ]
     },
-    {
-      name: "2. Naturalistic Buddhism（自然主義的仏教）",
-      summary: "ギル・フロンズダールによる、ブッダの初期教義に対する現代的かつ緻密に論考された解釈に基づく実践。同センターにおける彼のメディアプレゼンテーションやその他のサービスへのリンクを提供しています。",
-      image: "/gil.png",
-      links: [
-        { text: "インサイト・センター", href: "https://www.insightmeditationcenter.org/" },
-        { text: "ギルによるオーディオ・ダルマ", href: "https://www.audiodharma.org/speakers/1" }
-      ]
-    },
-    {
-      name: "3. Doug’s Dharma（ダグズ・ダルマ）",
-      summary: "世俗的、歴史的、および学術的な観点から仏教テキストの広範な動画分析を行っています。ダグの動画の一部は世俗仏教に焦点を当てており、また別の動画では伝統的な教えや、それらと哲学思想との結びつきを検証しています。",
-      image: "/doug.png",
-      links: [
-        { text: "ウェブサイト", href: "https://www.dougsdharma.com/" },
-        { text: "YouTubeチャンネル", href: "https://www.youtube.com/c/dougsdharma" }
-      ]
-    },
-    {
-      name: "4. Noah Rasheta（ノア・ラシェータ）",
-      summary: "「Secular Buddhism Podcast」のホストであり、AI搭載チャットを通じてアクセスできる仏教情報リソースを豊富に備えたデータベースを開発しています。",
-      image: "/noah.png",
-      links: [
-        { text: "八正道（8-Fold Path）", href: "https://eightfoldpath.com" },
-        { text: "ポッドキャスト", href: "https://secularbuddhism.com/" }
-      ]
-    },
-    {
-      name: "5. Secular Buddhist Tradition（SBT / 世俗仏教伝統）",
-      summary: "テンジン・パルパ尊師による瞑想センターのネットワーク。宗教的な権威、教条（ドグマ）、儀礼から離れ、倫理と相互理解へと向かうことに焦点を当てています。",
-      image: "/sbt.png",
-      links: [
-        { text: "SBTを訪問", href: "https://sbtonline.org/" },
-        { text: "SBTについて", href: "https://sbtonline.org/explore-secular-buddhism-guide/" }
-      ]
-    },
-    {
-      name: "6. Navayana Buddhism（ナヴァヤーナ仏教）",
+     {
+      name: "2. ナヴァヤーナ仏教 (Navayana Buddhism）",
       summary: "B.R.アンベードカル博士が提唱した「新乗り物（新仏教）」。科学と互換性があり、平等主義的な改革に合致した市民文化を支えるのに適した倫理として、ブッダの教えを再解釈した歴史的先例です。",
       image: "/ambedkar.jpg",
       links: [
@@ -80,25 +44,62 @@ export const SITE_CONTENT: SiteContent = {
       ]
     },
     {
-      name: "7. 法華伝統における世俗仏教",
-      summary: "法華経のクライマックスである「久遠実成の仏（永遠のブッダ）」の開顕を、内在に根ざした普遍的倫理として再解釈し、それに対応する世俗的実践のための祈りを紹介しています。",
-      image: "/lotus.png",
+      name: "3. 世俗仏教ネットワーク (The Secular Buddhist Network）",
+      summary: "スティーブン・バチェラーやウィントン・ヒギンズの基礎的な著作に触発され、現代における「世俗的なダルマ（法）」への転換を牽引しています。その活動グループは欧米諸国に広がっています。",
+      image: "/sbn.png", 
       links: [
-        { text: "Secular Lotus（英語版）", href: "https://secular-lotus.vercel.app/" },
-        { text: "日本語サイト", href: "https://sezoku-hokke.vercel.app/" }
+        { text: "公式サイト", href: "https://secularbuddhistnetwork.org/" },
+        { text: "コミュニティ", href: "https://secularbuddhistnetwork.org/the-sbn-community/" }
       ]
     },
+    {
+      name: "4. 自然主義的仏教 (Naturalistic Buddhism）",
+      summary: "ギル・フロンズダールによる、ブッダの初期教義に対する現代的かつ緻密に論考された解釈に基づく実践。同センターにおける彼のメディアプレゼンテーションやその他のサービスへのリンクを提供しています。",
+      image: "/gil.png",
+      links: [
+        { text: "インサイト・センター", href: "https://www.insightmeditationcenter.org/" },
+        { text: "ギルによるオーディオ・ダルマ", href: "https://www.audiodharma.org/speakers/1" }
+      ]
+    },
+    {
+      name: "5. ダグズ・ダルマ (Doug’s Dharma）",
+      summary: "世俗的、歴史的、および学術的な観点から仏教テキストの広範な動画分析を行っています。ダグの動画の一部は世俗仏教に焦点を当てており、また別の動画では伝統的な教えや、それらと哲学思想との結びつきを検証しています。",
+      image: "/doug.png",
+      links: [
+        { text: "ウェブサイト", href: "https://www.dougsdharma.com/" },
+        { text: "YouTubeチャンネル", href: "https://www.youtube.com/c/dougsdharma" }
+      ]
+    },
+    {
+      name: "6. ノア・ラシェータ (Noah Rasheta）",
+      summary: "「Secular Buddhism Podcast」のホストであり、AI搭載チャットを通じてアクセスできる仏教情報リソースを豊富に備えたデータベースを開発しています。",
+      image: "/noah.png",
+      links: [
+        { text: "八正道（8-Fold Path）", href: "https://eightfoldpath.com" },
+        { text: "ポッドキャスト", href: "https://secularbuddhism.com/" }
+      ]
+    },
+    {
+      name: "7. 世俗仏教伝統 (Secular Buddhist Tradition / SBT）",
+      summary: "テンジン・パルパ尊師による瞑想センターのネットワーク。宗教的な権威、教条（ドグマ）、儀礼から離れ、倫理と相互理解へと向かうことに焦点を当てています。",
+      image: "/sbt.png",
+      links: [
+        { text: "SBTを訪問", href: "https://sbtonline.org/" },
+        { text: "SBTについて", href: "https://sbtonline.org/explore-secular-buddhism-guide/" }
+      ]
+    },
+    
     {
       name: "基本参考資料",
       summary: "",
       image: "/book.png",
       links: [
-        { text: "スティーブン・バチェラー論文：「世俗仏教（A Secular Buddhism）」", href: "https://www.globalbuddhism.org/article/view/1189/1024"},
-        { text: "ウィントン・ヒギンズ：「世俗仏教の到来（The Coming of Secular Buddhism）」", href: "https://www.globalbuddhism.org/article/view/1188/1023" },
-        { text: "ギル・フロンズダール論文：「自然主義的仏教（Natural Buddhism）」", href: "https://www.insightmeditationcenter.org/books-articles/natural-buddhism/" },
+        { text: "スティーブン・バチェラー論文：「世俗仏教」", href: "https://www.globalbuddhism.org/article/view/1189/1024"},
+        { text: "ウィントン・ヒギンズ：「世俗仏教の到来」", href: "https://www.globalbuddhism.org/article/view/1188/1023" },
+        { text: "ギル・フロンズダール論文：「自然主義的仏教」", href: "https://www.insightmeditationcenter.org/books-articles/natural-buddhism/" },
         { text: "聖職者によるギル・フロンズダールへのインタビュー動画", href: "https://www.youtube.com/watch?v=CvFMx_HZ_nc" },
         { text: "ダグ・スミスによる解説：アンベードカル博士の世俗仏教について", href: "https://www.youtube.com/watch?v=qlH_qieCgCA" },
-        { text: "ノア・ラシェータ記事：「私たちはみな下流にいる（We're All Downstream）」", href: "https://secularbuddhistnetwork.org/were-all-downstream/"},
+        { text: "ノア・ラシェータ記事：「私たちはみな下流にいる」", href: "https://secularbuddhistnetwork.org/were-all-downstream/"},
         { text: "SBTによる世俗仏教入門ガイド", href: "https://drive.google.com/file/d/1Dkaz8yHsuKKIjDwhTRFRn8CgEXCYaUMW/view"},
         { text: "法華経に基づく世俗仏教（論文）", href: "https://zenodo.org/records/20076233"}   
       ]
